e12ce0a5de5154a3d4dbc9136281e56a626f1659
Adam Pod <adam.podoxin@gmail.com>
2018-10-09 16:56:41 -0700

    Created "Song" ScriptableObject
    -public AudioClip audio
    -public new string name = "New Song"
    -public int bpm

---
+++ Assets/Songs/Song.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Song", menuName = "Song")]
+public class Song : ScriptableObject
+{
+    public AudioClip audio;
+    public new string name = "New Song";
+    public int bpm;
+}
