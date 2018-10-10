a87a49d24e5ba42afaa62e3bfd7d85e40c4b6fce
Adam Pod <adam.podoxin@gmail.com>
2018-10-10 15:43:43 -0700

    New movement code cleanup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Vector2 moveBounds;
-    public float sensitivity;
+
+    private float timeFromLastMove = 0f;
 
     private void Update()
     {
-        //PROOF OF CONCEPT TO BE CHANGED || Only checks the input twice instead of 4 times
-        var xAxis = Input.GetAxis("Horizontal");
-        var yAxis = Input.GetAxis("Vertical");
-        
-        //feels diffrent than other solution
-        MovePlayer(yAxis < -sensitivity ? Vector2.down : Vector2.zero);
-        MovePlayer(yAxis > sensitivity  ? Vector2.up  : Vector2.zero);
-        MovePlayer(xAxis < -sensitivity ? Vector2.left : Vector2.zero);
-        MovePlayer(xAxis > sensitivity  ? Vector2.right : Vector2.zero);
+        timeFromLastMove -= Time.deltaTime;
 
-      
+        //PROOF OF CONCEPT TO BE CHANGED || Only checks the input twice instead of 4 times
+        var xAxis = Input.GetAxisRaw("Horizontal");
+        var yAxis = Input.GetAxisRaw("Vertical");
+
+        //feels diffrent than other solution
+        if (timeFromLastMove <= 0f)
+        {
+            MovePlayer(new Vector2(xAxis, yAxis));
+        }
+
+
         //if (Input.GetKeyDown(KeyCode.W))
         //{
         //    MovePlayer(Vector2.up);
@@ -43,13 +46,14 @@ public class PlayerMovement : MonoBehaviour
 
     public void MovePlayer(Vector2 move)
     {
-
         Vector3 newPosition = transform.position + new Vector3(move.x, move.y, 0);
 
         if (newPosition.x >= -moveBounds.x && newPosition.x <= moveBounds.x && newPosition.y >= -moveBounds.y && newPosition.y <= moveBounds.y)
         {
             transform.position = newPosition;
         }
+
+        timeFromLastMove = 0.15f;
     }
 
     public void MovePlayerTo(Vector2 newPosition)
