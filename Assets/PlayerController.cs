af08f342adcf252b0954087be83734d0f17fa74b
Shahan Neda <shahan.neda@gmail.com>
2018-10-11 21:03:56 -0700

    Added PlayerController, and its Error messeges and refactored Game Controller

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [HideInInspector]
+    public PlayerMovement playerMovement;
+	// Use this for initialization
+	void Start () {
+        playerMovement = GetComponent<PlayerMovement>();
+        if(playerMovement == null){
+            throw new MissingReferenceException("Please add PlayerMovement to player!!");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,11 +6,21 @@ public class GameController : MonoBehaviour
 {
     public static GameController instance;
 
-    public PlayerMovement playerMovement;
+    public PlayerController playerController;
     public SongController songController;
 
     private void OnEnable()
     {
         instance = this;
+    }
+    private void Start()
+    {
+        if(playerController == null){
+            throw new MissingReferenceException("Please add playerController to game controller!!");
+        }
+        if (songController == null)
+        {
+            throw new MissingReferenceException("Please add songController to game controller!!");
+        }
     }
 }
