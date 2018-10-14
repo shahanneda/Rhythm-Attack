e9bb318557ea05058c767ef91f6d9e49ecbb60b8
Shahan Neda <shahan.neda@gmail.com>
2018-10-13 20:10:21 -0700

    Added bullet manager

---
+++ Assets/BulletManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour {
+    /*This is what im thinking of it will read a text like this
+     *  e e e e e e e e e e e b e e e,
+     *  e e e e e e e b e e b e e e e,
+     *  e e e e e e b b e b e e e e e,
+     *  e e e e e b e b b e e e e e e,
+     *  e b e e e e b b e e e e e e e,
+     *  e e e e e e e b e e e e e e e.
+     *  e e e e b e b b e e e e e e e,
+     *  e e e b e e e e e e e e e e e,
+     *  e e b e e e e e e e e e e e e,
+     *  e e e e e e e e e e e e e e e
+     * 
+     * 
+     * and it would spanw a bullet everywhere there is an B 
+     * and e are just empty, and each pattern would have like 15 of these.
+     */
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
