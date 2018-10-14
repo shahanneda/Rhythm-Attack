78926a703d6dd32ee17e013c2b393f173ed0f299
Shahan Neda <shahan.neda@gmail.com>
2018-10-13 20:47:52 -0700

    got bullet on and off partly working

--- Assets/BulletManager.cs
+++ Assets/BulletManager.cs
@@ -20,32 +20,34 @@ public class BulletManager : MonoBehaviour {
      * and e are just empty, and each pattern would have like 15 of these.
      */
 
-    public char[ , ] bulletPattern1 = new char[16 , 16];
+    public char[ , ] bulletPattern1 = new char[15 , 15];
     // Use this for initialization
     void Start () {
    
-
-        for (int x = 0; x < 16 ; x++){
-            for (int y = 0; y < 16; y++){
+      
+        for (int x = 0; x < 15 ; x++){
+            for (int y = 0; y < 15; y++){
                 bulletPattern1[x , y] = 'r';
             }
         }
 
 
+       
 
     }
 	
 	// Update is called once per frame
 	void Update () {
         if(Input.GetKey(KeyCode.L)){
-            GameController.instance.gridGenerator.bulletGrid[2, 2].gameObject.SetActive(false);
+            //GameController.instance.gridGenerator.bulletGrid[2, 2].gameObject.SetActive(false);
+            LoadBulletPattern();
         }
 	}
 
     public void LoadBulletPattern(){
-        for (int x = 0; x < 16; x++)
+        for (int x = 0; x < 15; x++)
         {
-            for (int y = 0; y < 16; y++)
+            for (int y = 0; y < 15; y++)
             {
                 if(bulletPattern1[x,y] == 'r'){
                     GameController.instance.gridGenerator.bulletGrid[x, x].gameObject.SetActive(true);
