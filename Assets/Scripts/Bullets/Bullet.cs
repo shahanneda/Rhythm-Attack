e2b554c01f5518bf967addf1643f0a9b29434909
Adam Pod <adam.podoxin@gmail.com>
2018-11-02 21:50:06 -0700

    Created green bullets and smooth bullet movement

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -8,18 +8,27 @@ public class Bullet : MonoBehaviour
 
     private GridGenerator gridGenerator;
 
-    private void Start()
+    private Vector3 newPos;
+
+    public void Start()
     {
+        newPos = transform.position;
+
         GameController.instance.songController.beat += Move;
 
         gridGenerator = GameController.instance.gridGenerator;
     }
 
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, newPos, Time.deltaTime * 25f);
+    }
+
     public void Move()
     {
-        transform.position += new Vector3(bulletStats.direction.x, bulletStats.direction.y, 0);
+        newPos += new Vector3(bulletStats.direction.x, bulletStats.direction.y, 0);
 
-        if (transform.position.x >= gridGenerator.size.x - gridGenerator.xHalf || transform.position.y >= gridGenerator.size.y - gridGenerator.yHalf || transform.position.x <= (gridGenerator.size.x - gridGenerator.xHalf) * -1 || transform.position.y <= (gridGenerator.size.y - gridGenerator.yHalf) * -1)
+        if (newPos.x >= gridGenerator.size.x - gridGenerator.xHalf || newPos.y >= gridGenerator.size.y - gridGenerator.yHalf || newPos.x <= (gridGenerator.size.x - gridGenerator.xHalf) * -1 || newPos.y <= (gridGenerator.size.y - gridGenerator.yHalf) * -1)
         {
             GameController.instance.songController.beat -= Move;
             Destroy(gameObject);
---
+++ Assets/Scripts/Bullets/GreenBullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenBullet : Bullet
+{
+    private GameController gameController;
+    private PlayerMovement playerMovement;
+
+    private void Awake()
+    {
+        gameController = GameController.instance;
+        playerMovement = gameController.playerController.playerMovement;
+
+        gameController.songController.beat += FollowPlayer;
+    }
+
+    private void FollowPlayer()
+    {
+        Vector2 direction = playerMovement.transform.position - transform.position;
+
+        if (direction.x > 1)
+        {
+            direction.x = 1;
+        }
+        else if (direction.x < -1)
+        {
+            direction.x = -1;
+        }
+
+        if (direction.y > 1)
+        {
+            direction.y = 1;
+        }
+        else if (direction.y < -1)
+        {
+            direction.y = -1;
+        }
+
+        bulletStats.direction = direction;
+    }
+}
