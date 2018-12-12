404775f4f54b27912cbb7f250dab9a97a3393dae
Adam Pod <adam.podoxin@gmail.com>
2018-12-11 17:53:01 -0800

    Removed batteries taking damage on trigger enter

--- Assets/Scripts/Battery.cs
+++ Assets/Scripts/Battery.cs
@@ -2,32 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Battery : MonoBehaviour {
+public class Battery : MonoBehaviour
+{
     //TODO: Make battries spawn from the level editor
     // Use this for initialization
     public int Health = 3;
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    public void OnAttack(){
+
+    public void OnAttack()
+    {
         //TODO: Add Animations for the the death
         //TODO: Add multiple health states with diffrent sprites for each
         Health--;
-        if(Health <= 0){
+        if (Health <= 0)
+        {
             Destroy(gameObject);
         }
-       
+
     }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        OnAttack();
-    }
-
-
 }
