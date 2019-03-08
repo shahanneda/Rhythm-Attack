114b88f7b00251cc40ef81b3ac3539f4b2e0f975
Adam Pod <adam.podoxin@gmail.com>
2019-03-07 21:53:25 -0800

    Fixed attacks not registering on some areas of boss

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -83,8 +83,8 @@ public class PlayerController : MonoBehaviour
         }
 
         Vector3 attackPosition = transform.position + new Vector3(playerMovement.lastDirectionMoved.x, playerMovement.lastDirectionMoved.y, transform.position.z);
-        print(playerMovement.lastDirectionMoved);
         Battery battery = FindObjectOfType<BulletSpawner>().GetBatteryAtPosition(attackPosition);
+
         if (battery != null)
         {
             battery.OnAttack();
@@ -160,7 +160,7 @@ public class PlayerController : MonoBehaviour
 
     public static bool BossAtPosition(Vector3 position)
     {
-        if (position == Vector3.up || position == Vector3.down || position == new Vector3(-2, -2) || position == Vector3.left * 2 || position == new Vector3(-2, 1) || position == new Vector3(-2, 2) || position == new Vector3(1, 2) || position == new Vector3(2, 2) || position == Vector3.right * 2 || position == new Vector3(2, -1) || position == new Vector3(2, -2) || position == Vector3.down * 2 || position == new Vector3(-1, -2))
+        if ((position == Vector3.zero || position == Vector3.up || position == new Vector3(1, 1) || position == Vector3.right || position == new Vector3(1, -1) || position == Vector3.down || position == new Vector3(-1, -1) || position == Vector3.left || position == new Vector3(-1, 1)) && FindObjectOfType<Boss>() != null)
             return true;
         else
             return false;
